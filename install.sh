ab11b7031cb2a828e248930d53966c892b640d2c
Pritesh Ranjan <pranjan341@gmail.com>
2018-01-24 20:12:57 +0530

    run as bash install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 echo "installing dependencies via apt package manager"
 sudo apt-get install python3-pyqt5
+chmod u+x NoteNote.py
+sudo install NoteNote.py /usr/local/bin/NoteNote
 mkdir ~/.NoteNote
+cp icons/ ~/.NoteNote
+cd ~/.NoteNote
+echo "[Desktop Entry]" > NoteNote.desktop
+echo "Version=1.0" >> NoteNote.desktop
+echo "Name=NoteNote" >> NoteNote.desktop
+echo "Comment="NoteNote Sticky Notes"" >> NoteNote.desktop
+echo "Exec=NoteNote" >> NoteNote.desktop
+echo "Path=/home/prites/projects/NoteNote/" >> NoteNote.desktop
+echo "Icon=/home/prites/projects/NoteNote/icons/letter-2.gif" >> NoteNote.desktop
+echo "Terminal=false" >> NoteNote.desktop
+echo "Type=Application" >> NoteNote.desktop
+echo "Categories=Utility;" >> NoteNote.desktop
+cp NoteNote.desktop ~/.local/share/applications
