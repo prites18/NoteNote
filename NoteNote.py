9f3a75c3b3ffcebeb3527a8c23d4be66b5a1a6d8
Pritesh Ranjan <pranjan341@gmail.com>
2018-01-25 09:36:34 +0530

    added appmanager class

--- NoteNote.py
+++ NoteNote.py
@@ -8,6 +8,7 @@ import sys
 import os
 import random
 import getpass
+import glob
 
 from PyQt5.QtWidgets import (QApplication, QTextEdit, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel)
 from PyQt5.QtGui import QIcon, QFont
@@ -31,7 +32,9 @@ class NoteNote(QWidget):
         self.lck_btn = QPushButton()
         self.lck_btn.setToolTip("Turn on Protection")
         self.lck_btn.setIcon(QIcon("icons/lock.png"))
-        #self.pin_btn = QPushButton("PIN")
+        self.new_btn = QPushButton()
+        self.new_btn.setToolTip("New notes")
+        self.new_btn.setIcon(QIcon("icons/plus.png"))
         self.gen_ui()
 
     def gen_ui(self) -> None:
@@ -40,16 +43,16 @@ class NoteNote(QWidget):
         v_layout = QVBoxLayout()
         h_layout = QHBoxLayout()
         h_layout.addWidget(self.dmp_btn)
-        h_layout.addSpacing(50)
+        h_layout.addSpacing(45)
         h_layout.addWidget(self.title)
         h_layout.addSpacing(50)
         h_layout.addWidget(self.lck_btn)
-        #h_layout.addWidget(self.pin_btn)
+        h_layout.addWidget(self.new_btn)
         v_layout.addLayout(h_layout)
         v_layout.addWidget(self.text)
         self.dmp_btn.clicked.connect(self.dmp_act)
         self.lck_btn.clicked.connect(lambda: self.lck_act(self.text.isReadOnly()))
-        #self.pin_btn.clicked.connect(self.pin_act)
+        self.new_btn.clicked.connect(self.new_act)
         self.setLayout(v_layout)
         self.text.setText(self.saved_data)
 
@@ -89,7 +92,7 @@ class NoteNote(QWidget):
             notes = self.text.toPlainText()
             nf.write(notes.encode("utf-8"))
 
-    def pin_act(self) -> None:
+    def new_act(self) -> None:
         """yet to be implemented"""
         pass
 
@@ -111,11 +114,28 @@ class NoteNote(QWidget):
         self.setWindowIcon(QIcon("icons/letter-0.png"))
         self.show()
 
+
+class AppManager:
+    """ Manages the window instances and loads the required data and programs at startup"""
+    def __init__(self) -> None:
+        self.req_wind = len(glob.glob1("./", "*.cfg"))
+        self.config_files = glob.glob("*.cfg")
+        self.user = getpass.getuser()
+        self.path = "/home/"+self.user+"/.NoteNote/"
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
+    def startup(self) -> None:
+        """ called when the app is restarted or opened for the first time"""
+        for file in self.config_files:
+            NoteNote(self.path+"notes.bin").main()
+
+    def new_window(self) -> None:
+        """ to initialize a new window"""
+        pass
+
+
 if __name__ == "__main__":
-    USER = getpass.getuser()
-    PATH = "/home/"+USER+"/.NoteNote/"
-    if not os.path.exists(PATH):
-        os.makedirs(PATH)
     APP = QApplication(sys.argv)
-    NOTES_APP = NoteNote(PATH+"notes.bin").main()
+    AppManager().startup()
     sys.exit(APP.exec_())
